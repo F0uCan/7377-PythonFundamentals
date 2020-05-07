647f02ddd5be987dfe8937f8b6be6835b2531399
Guilherme <guilherme.zanelato@4linux.com.br>
2020-05-06 22:20:23 -0300

    Introdução ao Python

---
+++ Aula01/ex01.py
@@ -0,0 +1,57 @@
+# Ex.01 
+
+# formatação de saída
+## método format
+## fstring
+
+# comportamentos de string
+exit()
+### 
+#and 
+print( 5>3 and 3<4) 
+
+#or
+print( 5>3 and 55 < 10) 
+
+exit()
+print(5>3) # maior
+print(3>10) 
+print(5==5) # igualdade
+print(3!=2) # diferença
+print(5 >= 10) # maior ou igual 
+print(6 <= 12) # menor ou igual
+
+teste = 5 == 5
+
+exit()
+num1 = 12
+num2 = 15
+
+print(num1+num2) 
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = float(input('Digite o segundo número: ')) 
+
+print(num1+num2)
+
+exit()
+num1 = input('Digite o primeiro número')
+num2 = input('Digite o segundo número') 
+
+print(num1+num2) # soma de strings significa colocar uma do lado da outra
+print(num1*2) 
+
+exit()
+# Escreva uma saudação que contenha o nome e a idade na tela
+
+## Entrada
+# 1: Python, capture as informações de nome e de idade
+nome = input('Digite o Seu nome: ')  # <-texto/String 
+idade = input('Digite a sua idade: ') 
+
+# 2: Saída
+print('Olá , meu nome é ', nome,  ' e tenho ', idade, ' anos.')
+
+
+
+
